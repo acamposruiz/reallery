795e8a04fb46beda8dd0557c16fe4adb9384930f
Antonio Campos <acamposruiz@gmail.com>
2017-03-20 15:38:47 +0100

    Generate responsive images

---
+++ gemerate_images.js
@@ -0,0 +1,78 @@
+// Require library
+var gd = require('node-gd');
+var randomColor = require('randomcolor');
+
+var colors = randomColor({
+    count: 10,
+    luminosity: 'light',
+    format: 'rgb'
+});
+
+
+colors = colors.map(color => {
+    return color.substring(4).split(',').map(val => {
+        return parseInt(val);
+    })
+});
+
+colors.forEach( (color, key) => {
+
+        var dimensions = [160];
+        dimensions.forEach(w => {
+            var h = Math.floor(w/2);
+
+            // Create blank new image in memory
+            var img = gd.createSync(w, h);
+
+// Set background color
+            img.colorAllocate(color[0], color[1], color[2]);
+
+// Set text color
+            var txtColor = img.colorAllocate(0, 0, 0);
+
+// Set full path to font file
+            var fontPath = 'assets/Noway-Regular-webfont/noway-regular-webfont.ttf';
+
+// Render string in image
+            img.stringFT(txtColor, fontPath, Math.floor(300*(h/1800)), 0, Math.floor(w/8), Math.floor(h/2), w+' x '+h);
+
+// Write image buffer to disk
+            img.savePng('projts/005/images/'+key+'_img'+ w+'x'+h+'.png', 1, function(err) {
+                if(err) {
+                    throw err;
+                }
+            });
+        });
+
+    }
+);
+
+/*
+ var dimensions = [2880, 1240, 620, 310, 160];
+dimensions.forEach(w => {
+    var h = Math.floor(w/2);
+
+    // Create blank new image in memory
+    var img = gd.createSync(w, h);
+
+// Set background color
+    img.colorAllocate(155, 200, 200);
+
+// Set text color
+    var txtColor = img.colorAllocate(0, 0, 0);
+
+// Set full path to font file
+    var fontPath = 'assets/Noway-Regular-webfont/noway-regular-webfont.ttf';
+
+// Render string in image
+    img.stringFT(txtColor, fontPath, Math.floor(300*(h/1800)), 0, Math.floor(w/8), Math.floor(h/2), w+' x '+h);
+
+// Write image buffer to disk
+    img.savePng('projts/005/images/img'+ w+'x'+h+'.png', 1, function(err) {
+        if(err) {
+            throw err;
+        }
+    });
+});
+*/
+
--- upstate.js
+++ upstate.js
@@ -1,12 +1,46 @@
 /* Import modules */
 var fs = require("fs");
 var sizeOf = require('image-size');
+var lwip = require('lwip');
+var rmdir = require('rmdir');
 
 /* Import JSON DATA */
 var projtsJson = JSON.parse(fs.readFileSync("projts/projts.json", "utf8"));
 
 /* Declaration of vars */
 var output;
+
+/* Generate responsive images */
+function generateSourceResponsive(file, imagesFolder, dmsns, sourceImagesDir) {
+    var dimensions = [2880, 1240, 620, 310, 160];
+    var srcset = [];
+    var dimTop;
+    var sourceImageDir = sourceImagesDir + file.split('.')[0] + '/';
+
+    if (!fs.existsSync(sourceImageDir)) fs.mkdirSync(sourceImageDir);
+
+    dimensions.some((d, key) => {
+        dimTop = key;
+        return dmsns.width >= d;
+    });
+
+    for (var i = dimTop; i <= 4; i++) {
+        var dim = dimensions[i];
+        var sourceImageDirFile = sourceImageDir + '/' + dim + '_' + file;
+
+        lwip.open(imagesFolder + file, function (err, image) {
+
+            image.batch()
+                .resize(this.dim, (this.dim/dmsns.width)*dmsns.height)
+                .writeFile(this.sourceImageDirFile, function (err) {
+                    srcset.push(this.sourceImageDirFile);
+                });
+
+        }.bind({sourceImageDirFile:sourceImageDirFile, dim:dim}));
+    }
+
+    return srcset;
+}
 
 
 /* Generate the general output */
@@ -18,17 +52,31 @@ output += "var projects = {};";
 Object.keys(projtsJson).forEach(function (key) {
     /* Declaration of vars */
     var imagesFolder = 'projts/'+key+'/images/';
+    var sourceImagesDir = 'projts/'+key+'/source_images/';
+
+    /*if(fs.existsSync(sourceImagesDir)) {
+        rmdir(sourceImagesDir, function (err, dirs, files) {
+            fs.mkdirSync(sourceImagesDir);
+        });
+    } else {
+        fs.mkdirSync(sourceImagesDir)
+    }*/
+
+    if (!fs.existsSync(sourceImagesDir)) fs.mkdirSync(sourceImagesDir);
 
     /* Get images info */
     var files = fs.readdirSync(imagesFolder);
     projtsJson[key]["images"] = [];
     files.forEach(file => {
-        var dimensions = sizeOf(imagesFolder + file);
-        projtsJson[key]["images"].push({
-            path:imagesFolder + file,
-            width:dimensions.width,
-            height:dimensions.height
-        });
+        if (file.split('.')[1] == 'jpg' || file.split('.')[1] == 'png') {
+            var dimensions = sizeOf(imagesFolder + file);
+            projtsJson[key]["images"].push({
+                path:imagesFolder + file,
+                srcset: generateSourceResponsive(file, imagesFolder, dimensions, sourceImagesDir),
+                width:dimensions.width,
+                height:dimensions.height
+            });
+        }
     });
 
     /* include project */
