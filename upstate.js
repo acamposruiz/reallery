3b7918c23e16441161a9fd68d203957336ee5a40
Antonio Campos <acamposruiz@gmail.com>
2017-03-25 22:13:53 +0100

    Making the state generator sequencial

--- upstate.js
+++ upstate.js
@@ -1,10 +1,13 @@
 /* Import modules */
 var fs = require("fs");
+/*import fs from 'fs';*/
 var sizeOf = require('image-size');
 var lwip = require('lwip');
 var rmdir = require('rmdir');
+var colors = require('colors');
 
 /* Import JSON DATA */
+var output;
 var projtsJson = JSON.parse(fs.readFileSync("projts/projts.json", "utf8"));
 
 /* Declaration of vars */
@@ -32,6 +35,8 @@ function generateSourceResponsive(file, imagesFolder, dmsns, sourceImagesDir) {
 
                 lwip.open(imagesFolder + file, function (err, image) {
 
+                    console.log('Generating -> ' + imagesFolder + file + ' to width = ' + this.dim);
+
                     image.batch()
                         .resize(this.dim, (this.dim/dmsns.width)*dmsns.height)
                         .writeFile(this.sourceImageDirFile, function (err) {
@@ -49,57 +54,97 @@ function generateSourceResponsive(file, imagesFolder, dmsns, sourceImagesDir) {
 
 /* Generate the projects output */
 Object.keys(projtsJson).forEach(function (key) {
-    ps.push(new Promise(function(rslv, rjct) {
-        /* Declaration of vars */
-        var imagesFolder = 'projts/'+key+'/images/';
-        var sourceImagesDir = 'projts/'+key+'/source_images/';
+    ps.push(  ()=>{
+        return new Promise(function(rslv, rjct) {
+            /* Declaration of vars */
+            var imagesFolder = 'projts/'+key+'/images/';
+            var sourceImagesDir = 'projts/'+key+'/source_images/';
 
-        fs.existsSync(sourceImagesDir)
-            ? rmdir(sourceImagesDir, () => generateSourceImages(sourceImagesDir))
-            : generateSourceImages(sourceImagesDir);
+            fs.existsSync(sourceImagesDir)
+                ? rmdir(sourceImagesDir, () => generateSourceImages(sourceImagesDir))
+                : generateSourceImages(sourceImagesDir);
 
-        function generateSourceImages(sourceImagesDir){
-            fs.mkdirSync(sourceImagesDir);
+            function generateSourceImages(sourceImagesDir){
 
-            /* Get images info */
-            var files = fs.readdirSync(imagesFolder);
-            var imagesPromises = [];
+                fs.mkdirSync(sourceImagesDir);
+                console.log(('Created/Updated directory: ' + sourceImagesDir).cyan);
 
-            files.forEach(file => {
-                if (file.split('.')[1] == 'jpg' || file.split('.')[1] == 'png') {
-                    imagesPromises.push(new Promise(function(rslv2, rjct2) {
-                        var dimensions = sizeOf(imagesFolder + file);
-                        generateSourceResponsive(file, imagesFolder, dimensions, sourceImagesDir).then(srcset => {
-                            rslv2({
-                                path:imagesFolder + file,
-                                srcset: srcset,
-                                width:dimensions.width,
-                                height:dimensions.height
+                /* Get images info */
+                var files = fs.readdirSync(imagesFolder);
+                var imagesPromises = [];
+
+                files.forEach(file => {
+                    if (file.split('.')[1] == 'jpg' || file.split('.')[1] == 'png') {
+                        imagesPromises.push(new Promise(function(rslv2, rjct2) {
+                            var dimensions = sizeOf(imagesFolder + file);
+                            generateSourceResponsive(file, imagesFolder, dimensions, sourceImagesDir).then(srcset => {
+
+                                rslv2({
+                                    path:imagesFolder + file,
+                                    srcset: srcset,
+                                    width:dimensions.width,
+                                    height:dimensions.height
+                                });
                             });
-                        });
 
-                    }));
-                }
-            });
+                        }));
+                    }
+                });
 
-            Promise.all(imagesPromises).then(values => {
-                projtsJson[key]["images"] = values;
+                Promise.all(imagesPromises).then(values => {
 
-                /* include project */
-                var textCode = 'projects["' + key + '"]=' + JSON.stringify(projtsJson[key]) + ';';
-                rslv(textCode);
-            });
+                    projtsJson[key]["images"] = values;
+
+                    /* include project */
+                    var textCode = 'projects["' + key + '"]=' + JSON.stringify(projtsJson[key]) + ';';
 
 
-        }
 
-    }));
+                    rslv(textCode);
+                });
+
+            }
+
+        })
+    }  );
 });
 
-Promise.all(ps).then(values => {
-    /* Generate the final output */
+(function promRecur(index) {
+
+    if (index == 0) {
+        output = "var app = app || {};";
+        output += "(function () { 'use strict';";
+        output += "var projects = {};";
+    }
+
+    ps[index]().then(value => {
+
+        output += value;
+
+        console.log(('Generated raw code of project with key: ' + (index + 1)).green);
+
+        if(++index < ps.length){
+            promRecur(index);
+        } else {
+            output += "app.state = { projects: projects};";
+            output += "})();";
+            /* Write the output */
+            fs.writeFile("js/state.js", output, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+
+                console.log("The code was written!");
+            });
+        }
+    });
+
+})(0);
+
+/*Promise.all(ps).then(values => {
+    /!* Generate the final output *!/
     var output;
-    /* Generate the general output */
+    /!* Generate the general output *!/
     output = "var app = app || {};";
     output += "(function () { 'use strict';";
     output += "var projects = {};";
@@ -107,7 +152,7 @@ Promise.all(ps).then(values => {
     output += "app.state = { projects: projects};";
     output += "})();";
 
-    /* Write the output */
+    /!* Write the output *!/
     fs.writeFile("js/state.js", output, function (err) {
         if (err) {
             return console.log(err);
@@ -115,5 +160,7 @@ Promise.all(ps).then(values => {
 
         console.log("The code was written!");
     });
-});
+});*/
+
+/*readConfigData("projts/projts.json").then(generateSourceImages).then(writeRawState);*/
 
