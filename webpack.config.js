b7be1d13bfb4a05c90376a3f9ebb0c26857d0c8f
Antonio Campos <acamposruiz@gmail.com>
2017-03-26 12:51:10 +0200

    Update Webpack to 2 version

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 
 module.exports = {
 	resolve: {
-		extensions: ['', '.jsx', '.js', 'es6']
+		extensions: ['.jsx', '.js', 'es6']
 	},
 	entry:  './es6/app.es6',
 	output: {
-		path: './build',
+		path: __dirname + '/build',
 		filename:'app.js'
 	},
 	module: {
 		loaders: [
 			{
 				test: /(\.js|\.jsx|\.es6)$/,
-				loader: 'babel',
+				loader: 'babel-loader',
                 query: {
                     plugins: ['transform-decorators-legacy', "transform-class-properties", "syntax-decorators"],
                     presets: ['babel-preset-es2015', 'babel-preset-stage-0', 'babel-preset-react']
