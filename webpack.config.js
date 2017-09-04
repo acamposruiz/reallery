b054642da59d9491c1a9ecc5f15c4b829355beac
Antonio Campos <acamposruiz@gmail.com>
2017-09-04 10:40:27 +0200

    Code styles

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,27 @@ var path = require('path');
 
 module.exports = {
     devtool: "source-map",
-	resolve: {
-		extensions: ['.jsx', '.js', 'es6', 'css', 'pcss'],
+    resolve: {
+        extensions: ['.jsx', '.js', 'es6', 'css', 'pcss'],
         alias: {
             Styles: path.resolve(__dirname, 'src/styles/')
         }
-	},
-	entry:  './src/es6/app.es6',
-	output: {
-		path: __dirname + '/build',
-		filename:'app.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /(\.js|\.jsx|\.es6)$/,
-				loader: 'babel-loader',
+    },
+    entry:  './src/es6/app.es6',
+    output: {
+        path: __dirname + '/build',
+        filename:'app.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /(\.js|\.jsx|\.es6)$/,
+                loader: 'babel-loader',
                 query: {
                     plugins: ['transform-decorators-legacy', "transform-class-properties", "syntax-decorators"],
                     presets: ['babel-preset-es2015', 'babel-preset-stage-0', 'babel-preset-react']
                 }
-			},
+            },
             { test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/, exclude: /node_modules/, loader: 'url-loader?limit=100000' },
             {
                 test: /(\.css|\.pcss)$/,
@@ -47,6 +47,6 @@ module.exports = {
                     }
                 ]
             }
-		]
-	}
+        ]
+    }
 };
