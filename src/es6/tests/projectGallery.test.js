ece9a2b7703df3403c7335c3ae31aadd9467fc2f
Antonio Campos <acamposruiz@gmail.com>
2017-11-18 18:37:05 +0100

    [UNIT-TESTS] [SNAPSHOTS] Unit test to projectGallery component

---
+++ src/es6/tests/projectGallery.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProjectGallery from '../projectGallery';
+import renderer from 'react-test-renderer';
+import state from './mocks/state';
+
+jest.mock('../contentIcons', () => { return 'ContentIcons'; });
+jest.mock('react-multimedia-gallery', () => { return 'Gallery'; });
+
+test('ProjectGallery', () => {
+    const component = renderer.create(
+      <ProjectGallery lng="en" project={state.projects.architecture}/>
+    );
+    let tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+});
