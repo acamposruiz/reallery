2f8c1445ac5fc9257a5c05d2063f4784edd5ae78
Antonio Campos <acamposruiz@gmail.com>
2017-12-19 18:20:16 +0100

    [E2E-TESTS] [CYPRESS] --- Basic screenshots implementation

--- cypress/integration/multilanguage/scroll.js
+++ cypress/integration/multilanguage/scroll.js
@@ -12,11 +12,13 @@ describe('Scroll', function() {
         }
         it('Visits some page content', function() {
           cy.visit('http://127.0.0.1:8080/');
+          cy.screenshot(data.describe+'_homepage');
           cy.contains('Michelangelo Buonarroti');
           cy.get('.home-project-button-paintings>a').click();
           cy.get('#Gallery > div[data-type]').should('have.length', isBigScreen()? 20: 10);
           cy.get(`#Gallery > div:nth-child(${ isBigScreen()? 20: 10})`).trigger('mousedown');
           cy.wait(500);
+          cy.screenshot(data.describe+'_painting_page');
           cy.get('#Gallery > div[data-type]').should('have.length',  isBigScreen()? 27: 20);
           cy.get(`#Gallery > div:nth-child(${ isBigScreen()? 27: 20})`).trigger('mousedown');
           cy.wait(500);
@@ -31,6 +33,7 @@ describe('Scroll', function() {
           cy.get('#Gallery > div[data-type="video"]').should('have.length',  isBigScreen()? 4: 2);
           cy.get('#Gallery > div:nth-child(10)').trigger('mousedown');
           cy.wait(500);
+          cy.screenshot(data.describe+'_architecture_page');
           cy.get('#Gallery > div[data-type]').should('have.length', 15);
           cy.get('#Gallery > div:nth-child(15)').trigger('mousedown');
           cy.wait(500);
