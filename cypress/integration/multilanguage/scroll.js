0bce79819b5c8200f798e69871f7a02a4ba89907
Antonio Campos <acamposruiz@gmail.com>
2017-12-19 17:46:44 +0100

    [E2E-TESTS] [CYPRESS] [REFACTOR] --- Testing scroll functionality in big screen scenarios, refactor

--- cypress/integration/multilanguage/scroll.js
+++ cypress/integration/multilanguage/scroll.js
@@ -13,21 +13,6 @@ describe('Scroll', function() {
         it('Visits some page content', function() {
           cy.visit('http://127.0.0.1:8080/');
           cy.contains('Michelangelo Buonarroti');
-          cy.get('.home-project-button-architecture>a').click();
-          cy.get('#Gallery > div[data-type]').should('have.length', isBigScreen()? 15: 10);
-          cy.get('#Gallery > div[data-type="article"]').should('have.length', 3);
-          cy.get('#Gallery > div[data-type="photo"]').should('have.length',  isBigScreen()? 8: 5);
-          cy.get('#Gallery > div[data-type="video"]').should('have.length',  isBigScreen()? 4: 2);
-          cy.get('#Gallery > div:nth-child(10)').trigger('mousedown');
-          cy.wait(500);
-          cy.get('#Gallery > div[data-type]').should('have.length', 15);
-          cy.get('#Gallery > div:nth-child(15)').trigger('mousedown');
-          cy.wait(500);
-          cy.get('#Gallery > div[data-type]').should('have.length', 15);
-        });
-        it('Visits another page content', function() {
-          cy.visit('http://127.0.0.1:8080/');
-          cy.contains('Michelangelo Buonarroti');
           cy.get('.home-project-button-paintings>a').click();
           cy.get('#Gallery > div[data-type]').should('have.length', isBigScreen()? 20: 10);
           cy.get(`#Gallery > div:nth-child(${ isBigScreen()? 20: 10})`).trigger('mousedown');
@@ -39,6 +24,17 @@ describe('Scroll', function() {
           cy.get('#Gallery > div:nth-child(27)').trigger('mousedown');
           cy.wait(500);
           cy.get('#Gallery > div[data-type]').should('have.length', 27);
+          cy.get('div.project-button-container.home-project-button-architecture').click();
+          cy.get('#Gallery > div[data-type]').should('have.length', isBigScreen()? 15: 10);
+          cy.get('#Gallery > div[data-type="article"]').should('have.length', 3);
+          cy.get('#Gallery > div[data-type="photo"]').should('have.length',  isBigScreen()? 8: 5);
+          cy.get('#Gallery > div[data-type="video"]').should('have.length',  isBigScreen()? 4: 2);
+          cy.get('#Gallery > div:nth-child(10)').trigger('mousedown');
+          cy.wait(500);
+          cy.get('#Gallery > div[data-type]').should('have.length', 15);
+          cy.get('#Gallery > div:nth-child(15)').trigger('mousedown');
+          cy.wait(500);
+          cy.get('#Gallery > div[data-type]').should('have.length', 15);
         });
       });
     });
