c6d6597e475e68621d0626773e0fb98bf9209483
Antonio Campos <acamposruiz@gmail.com>
2019-01-08 19:51:34 +0100

    refactor: new compilations options to make tests optinal

--- internals/bundleConf/compilationPlugin.js
+++ internals/bundleConf/compilationPlugin.js
@@ -3,31 +3,43 @@ const {cyan} = require('../utils.js').colors;
 
 module.exports = class CompilationPlugin {
   constructor(options) {
-    this.options = options;
+    this.options = options || {};
   }
 
   apply(compiler) {
 
-    compiler.plugin('compile', function () {
-      console.log(cyan, 'RUNNING UNIT TESTS......');
-      cmd.get(
-        'npm run test',
-        function (err, data, stderr) {
-          console.log(data);
-          console.log(stderr);
-          console.log(err);
-        }
-      );
+    if(this.options.test){
+      compiler.plugin('compile', function () {
+        console.log(cyan, 'RUNNING UNIT TESTS......');
+        cmd.get(
+          'npm run test',
+          function (err, data, stderr) {
+            console.log(data, stderr, err);
+          }
+        );
+      });
+    }
+
+    compiler.plugin('beforeRun', function (...args) {
+      console.log(cyan, `MODE => ${args[0].options.mode}`);
     });
 
-    compiler.plugin('done', function () {
-      console.log(cyan, 'RUNNING E2E TESTS......');
+    compiler.plugin('done', () => {
+
+      if(this.options.test){
+        console.log(cyan, 'RUNNING E2E TESTS......');
+        cmd.get(
+          'npm run cypress:run',
+          function (err, data, stderr) {
+            console.log(data, stderr, err);
+          }
+        );
+      }
+
       cmd.get(
-        'npm run cypress:run',
+        "ls -lhS  build/* | awk '{print  $5, $9}'",
         function (err, data, stderr) {
-          console.log(data);
-          console.log(stderr);
-          console.log(err);
+          console.log(cyan, data, stderr, err);
         }
       );
     });
