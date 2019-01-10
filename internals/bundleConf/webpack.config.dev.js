35fd6b4b71db2b4d09b4e8620897afcd2d9842c8
Antonio Campos <acamposruiz@gmail.com>
2019-01-10 01:46:40 +0100

    chore(Webpack): agregate watch mode to dev webpack configuration

--- internals/bundleConf/webpack.config.dev.js
+++ internals/bundleConf/webpack.config.dev.js
@@ -7,6 +7,11 @@ module.exports = merge(baseConfig, {
   optimization: {
     minimize: false,
   },
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000,
+  },
   devtool: "inline-source-map",
   plugins: baseConfig.plugins(mode),
 });
--- internals/bundleConf/webpack.config.dev.watch.js
+++
@@ -1,9 +0,0 @@
-const merge = require("webpack-merge");
-const baseConfig = require("./webpack.config.dev");
-
-module.exports = merge(baseConfig, {
-  watch: true,
-  watchOptions: {
-    poll: 300000,
-  },
-});
